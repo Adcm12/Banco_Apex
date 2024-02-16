85ea40e884bde8e798b2b73e5877edde0be250cd
Adrian Castillo <david.marcanao07@gmail.com>
2024-02-16 18:39:30 -0300

    upload

--- Banco_Mercantil/Banco/views.py
+++ Banco_Mercantil/Banco/views.py
@@ -3,25 +3,11 @@ from rest_framework import viewsets
 from Banco.models import Conta_poupança, Conta_Corriente
 from .serializer import CorrienteSerializer, PoupançaSerializer
 
-# Create your views here.
-
-def conta_corriente(request):
-    if request.method == 'GET':
-        conta = {'Id': 1, 'Nome': 'Seu Nome'}
-        return JsonResponse(conta)
-    
-def conta_poupança(request):
-    if request.method == 'GET':
-        conta = {'Id': 1, 'Nome': 'Seu Nome'}
-        return JsonResponse(conta)
     
 class CorrienteViewSets(viewsets.ModelViewSet):
     "Exibindo todos os Cursos"
     queryset = Conta_Corriente.objects.all()
     serializer_class = CorrienteSerializer
-
-
-
 
 class PoupançaViewSets(viewsets.ModelViewSet):
     "Exibindo todos os Cursos"
